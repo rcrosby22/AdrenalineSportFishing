571d3bb825bd25d8d0cb4b00ec20e6dd715ebaf4
Ryan Crosby <ryan.crosby22@gmail.com>
2023-07-10 15:21:37 -0500

    component layouts

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  
+  return (
+    <nav className="navbar">
+      <h4>navbar</h4>
+      <div>
+        {/* create Links here */}
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
