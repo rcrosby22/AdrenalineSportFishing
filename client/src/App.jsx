c1a8858efe8da380f8e5dc4ccd448169b0d98319
Ryan Crosby <ryan.crosby22@gmail.com>
2023-07-11 10:57:35 -0500

    App importing/reviewForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 
 import './App.css'
 import BookingForm from './components/BookingForm'
+import Reviews from './components/Reviews'
+import About from './components/About'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Home from './components/Home'
+import Services from './components/Services'
+import ServiceDetails from './components/ServiceDetails'
 
 const App = () => {
 
 
   return (
       <div className="App">
+        <Header/>
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Services/>
+        <ServiceDetails/>
         <BookingForm/>
+        <Reviews/>
+        <Footer/>
 
       </div>
   )
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,7 +1,21 @@
+import React from "react"
+import { useState } from "react"
 
 const Reviews = () => {
+  const [reviewForm, setReviewForm] = useState({name:'', content:''})
+  const handleSubmitReview =(event)=> {
+    event.preventDefault()
+  }
   return(
     <div>
+      <form onSubmit={handleSubmitReview}>
+  <input type="text"
+   value={reviewForm.name} 
+   onChange={(event) => setReviewForm({ 
+    ...reviewForm, name: event.target.value })} />
+  <button type="submit">Submit Review</button>
+</form>
+   
 
     </div>
   )
