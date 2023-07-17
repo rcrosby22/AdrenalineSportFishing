3129db299a3984ea705c1d790406f7683476a05f
Ryan Crosby <ryan.crosby22@gmail.com>
2023-07-17 15:53:37 -0400

    military section photo and backdrop good

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
-import About from './About';
-import Services from '../components/Services';
-import Reviews from '../components/Reviews';
-import { Card, CardContent } from '@mui/material';
-import Gallery from '../components/Gallery';
-import image1 from '../assets/images/Adrenaline_sport_fishing/FB_IMG_1689166114728.jpg';
-import image2 from '../assets/images/Adrenaline_sport_fishing/FB_IMG_1689166211289.jpg';
-import image3 from '../assets/images/Adrenaline_sport_fishing/FB_IMG_1689166136455.jpg';
-import ReviewForm from '../components/ReviewForm/ReviewForm';
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
+import About from './About'
+import Services from '../components/Services'
+import Reviews from '../components/Reviews'
+import { Card, CardContent } from '@mui/material'
+import Gallery from '../components/Gallery'
+import image1 from '../assets/images/Adrenaline_sport_fishing/FB_IMG_1689166114728.jpg'
+import image2 from '../assets/images/Adrenaline_sport_fishing/FB_IMG_1689166211289.jpg'
+import image3 from '../assets/images/Adrenaline_sport_fishing/FB_IMG_1689166136455.jpg'
+import ReviewForm from '../components/ReviewForm/ReviewForm'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import imageMil from '/Users/rcrosby22/SEI-R-4-24/unit4/AdrenalineSportFishing/client/src/assets/images/Adrenaline_sport_fishing/IMG-20140827-170323.jpg'
 const Home = () => {
-  const photos = [image1, image2, image3];
+  const photos = [image1, image2, image3]
 
   return (
     <div>
@@ -30,8 +32,56 @@ const Home = () => {
       <br />
 
       <Gallery photos={photos} />
+      <Box
+        sx={{
+          backgroundColor: '#f0f0f0', // Set the background color
+          padding: '15rem' // Set the padding
+        }}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Box
+              sx={{
+                backgroundColor: 'creme',
+                padding: '1rem'
+              }}
+            ></Box>
+            {/* Content of the first section */}
+          </Grid>
+          <Grid item xs={6}>
+            {/* Content of the second section */}
+          </Grid>
+        </Grid>
+      </Box>
 
-     <ReviewForm/>
+      {/* Military section */}
+      <Box
+        sx={{
+          backgroundColor: 'rgba(0, 0, 50, 0.8)', // Set the background color
+          padding: '10rem' // Set the padding
+        }}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={6} sx={{ display: 'flex', alignItems: 'left' }}>
+            <img
+              src={imageMil}
+              alt="Adrenaline Sport Fishing"
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                marginLeft: '0'
+              }}
+            />
+            {/* Content of the first section */}
+          </Grid>
+          <Grid item xs={6}>
+            {/* Content of the second section */}
+          </Grid>
+        </Grid>
+      </Box>
+
+      <ReviewForm />
 
       <div>
         <div className="py-8 flex flex-col items-center">
@@ -42,7 +92,7 @@ const Home = () => {
 
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
