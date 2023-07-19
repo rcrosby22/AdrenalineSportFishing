5cb1da36cdc7d02cc8dae35ade0983b825628f35
Ryan Crosby <ryan.crosby22@gmail.com>
2023-07-19 00:50:02 -0400

    styled gallery

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,31 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Paper, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react'
+import { Grid, Paper, Typography } from '@mui/material'
 
+const imageStyle = {
+  padding: '10px',
+  transition: 'transform 0.2s ease-in-out',
+  '&:hover': {
+    transform: 'scale(1.05)'
+  },
+  maxWidth: '100%',
+  height: 'auto',
+  objectFit: 'cover',
+  borderRadius: '10px'
+}
 const Gallery = ({ photos }) => {
-  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0)
 
   const handleNext = () => {
-    setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length);
-  };
+    setCurrentPhotoIndex((prevIndex) => (prevIndex + 1) % photos.length)
+  }
 
   const handlePrev = () => {
-    setCurrentPhotoIndex((prevIndex) => (prevIndex - 1 + photos.length) % photos.length);
-  };
+    setCurrentPhotoIndex(
+      (prevIndex) => (prevIndex - 1 + photos.length) % photos.length
+    )
+  }
 
   useEffect(() => {
-    const timer = setInterval(handleNext, 5000); // Switch photo every 5 seconds
+    const timer = setInterval(handleNext, 5000) // Switch photo every 5 seconds
 
     return () => {
-      clearInterval(timer); // Clean up the timer on component unmount
-    };
-  }, []);
+      clearInterval(timer) // Clean up the timer on component unmount
+    }
+  }, [])
 
   return (
     <Grid container spacing={2} justifyContent="center">
       <Grid item>
-      <Typography variant="h3" align="center" gutterBottom>
+        <Typography variant="h3" align="center" gutterBottom>
           Our Past Adventures!
-        
         </Typography>
         <Paper elevation={3}>
           <img
@@ -33,21 +45,17 @@ const Gallery = ({ photos }) => {
             alt={`Photo ${currentPhotoIndex + 1}`}
             onClick={handleNext}
             onContextMenu={(e) => {
-              e.preventDefault();
-              handlePrev();
+              e.preventDefault()
+              handlePrev()
             }}
-            style={{
-              cursor: 'pointer',
-              maxWidth: '100%',
-              height: 'auto',
-              objectFit: 'cover', // Ensure the image fills the container
-              maxHeight: '300px' // Set a maximum height for consistent sizing
-            }}
+            
+            
+            style={imageStyle}
           />
         </Paper>
       </Grid>
     </Grid>
-  );
-};
+  )
+}
 
-export default Gallery;
+export default Gallery
