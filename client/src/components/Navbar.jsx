14f4839909c0b7794c362a5980559c66ad4dfb58
Ryan Crosby <ryan.crosby22@gmail.com>
2023-07-19 09:11:53 -0400

    appbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,49 +1,42 @@
-import { Link } from 'react-router-dom'
-import Grid from '@mui/material/Grid'
-import FacebookButton from './FacebookButton'
-import FacebookIcon from '@mui/icons-material/Facebook'
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, Grid } from '@mui/material';
+import FacebookButton from './FacebookButton';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 const Navbar = () => {
   return (
-    <Grid
-      container
-      component="nav"
-      alignItems="center"
-      justifyContent="space-between"
-      sx={{ py: 2 }}
-    >
-      <Grid item>
-        <FacebookButton />
-      </Grid>
-      <Grid item>
-        <ul className="nav-links" style={{ display: 'flex', gap: '1rem' }}>
-          <Link to="/booking" className="nav-link">
-            Book Now
-          </Link>
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container alignItems="center" justifyContent="space-between">
+          <Grid item>
+            <FacebookButton />
+          </Grid>
+          <Grid item>
+            <div className="nav-links" style={{ display: 'flex', gap: '1rem' }}>
+              <Button component={Link} to="/booking" color="inherit">
+                Book Now
+              </Button>
+              <Button component={Link} to="/user" color="inherit">
+                Update Account
+              </Button>
+              <Button component={Link} to="/" color="inherit">
+                Home
+              </Button>
+              <Button component={Link} to="/about" color="inherit">
+                About
+              </Button>
+              <Button component={Link} to="/register" color="inherit">
+                Register
+              </Button>
+              <Button component={Link} to="/signin" color="inherit">
+                Sign In
+              </Button>
+            </div>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-          <Link to="/user" className="nav-link">
-            Update Account
-          </Link>
-
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-
-          <Link to="/register" className="nav-link">
-            Register
-          </Link>
-
-          <Link to="/signin" className="nav-link">
-            Sign In
-          </Link>
-        </ul>
-      </Grid>
-    </Grid>
-  )
-}
-
-export default Navbar
+export default Navbar;
